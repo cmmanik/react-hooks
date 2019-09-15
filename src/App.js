859dc01a4be90a2ffa1ada4ac763bce30daa42ad
cmmanik <mkmanik6523u@gmail.com>
2019-09-15 15:17:33 +0600

    format search component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function App() {
+
+  const [serchQuery, setserchQuery] = useState('');
+  const cngHandler = event => {
+    setserchQuery(event.target.value)
+  }
+
+  const serch = () => {
+    window.open(`https://google.com/search?q=${serchQuery}`, '_blank')
+  }
+
+  const serchKeyPres = event => {
+    if(event.key === 'Enter') {
+      serch();
+    }
+  }
+
+  
   return (
     
       <div className="container ">
-       <h1>ok</h1>
+      <div className="form">
+        <input type="text" value={serchQuery} onKeyPress={serchKeyPres} onChange={cngHandler} />
+        <button onClick={serch}>Serch</button>
+      </div>
     </div>
   );
 }
